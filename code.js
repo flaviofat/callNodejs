6801ee551c88f52407daca2bf982b86a0c135337
Flavio Teixeira <flavio.teixeira@mercadopago.com.br>
2018-11-13 16:02:24 -0200

    init

---
+++ code.js
@@ -0,0 +1,30 @@
+const req = require('sync-request');
+const fs = require('fs');
+
+var obj = JSON.parse(fs.readFileSync('results.json', 'utf8'));
+obj.data.forEach(reprocessInvoice);
+console.log('Done!');
+
+function reprocessInvoice(obj) {
+    sleep(1000);
+
+    http://api.internal.ml.com/users/${obj.seller_id}/invoices/${obj.id}?caller.id=${obj.seller_id}
+    var retorno = req('GET', `http://api.internal.ml.com/users/${obj.seller_id}/invoices/${obj.id}?caller.id=${obj.seller_id}`);
+
+    //console.log(JSON.parse(retorno.body));
+
+    var result = JSON.parse(retorno.body);
+    console.log(result.shipment.logistic_type);
+    console.log(retorno.statusCode)
+
+
+}
+
+function sleep(milliseconds) {
+    var start = new Date().getTime();
+    for (var i = 0; i < 1e7; i++) {
+      if ((new Date().getTime() - start) > milliseconds){
+        break;
+      }
+    }
+}
